5a2bd36303ae27f1c96261ccc8c4e63a6bddcf58
AALabonya <1aklimakter@gmail.com>
2024-03-25 23:17:10 +0600

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Component/Router/Router";
-import AuthProvider from "./Provider/Authprovider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
